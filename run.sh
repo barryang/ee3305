fa4767e2d0fcee7f77166876c11c00b000570b86
barry <barryangbj2001@gmail.com>
2025-10-30 21:52:16 +0800

    run script
    /bash

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-#!/bin/
+#!/bin/bash
 echo "Running Build"
+cd ~/ee3305
 source install/setup.bash
 ros2 launch ee3305_bringup run.launch.py cpp:=False headless:=False libgl:=False
 ./kill.sh # run in case gz cannot be interrupted properly.
